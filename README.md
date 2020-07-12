f008a50ffaa66431fe4ea770856c62a7e163f4de
Daniel Da Costa <daniel.pereiracosta@hotmail.com>
2020-07-12 13:40:58 -0300

    Read_sav_from_s3

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # ml-production-deploy
 Deploying Machine Learning models on AWS using Serverless Framework
 
+# Details
+
+The input data will be stored in a S3 bucket, in our case the csv file. 
+
+The model file will also be stored inside a S3 bucket. Since the model file can be quite large (>90Mb), we will need to load it during AWS Lambda inference execution from Amazon S3. You can observe that the model downloading was placed outside of the handler function. This was done in order to take advantage of AWS Lambda container reuse. Any code executed outside of the handler method will be invoked only once upon container creation and kept in memory across calls to the same Lambda container, making subsequent calls to Lambda faster.
+
+
 # Usage
 
 In this repository will be using Python3.
---
+++ s3_utils.py
@@ -0,0 +1,35 @@
+import boto3
+import joblib
+import pandas as pd
+import os
+from tempfile import mkstemp
+
+
+def read_csv_from_s3(bucket, key, sep=','):
+    s3 = boto3.client('s3')
+    filedescriptor, csv = mkstemp(suffix='.csv')
+    with os.fdopen(filedescriptor, 'wb') as fileobj:
+        s3.download_fileobj(Bucket=bucket, Key=key, Fileobj=fileobj)
+    return pd.read_csv(csv, sep=sep, escapechar='\\', encoding='utf-8')
+
+
+def save_results(bucket, key, df, results, start_time):
+    date = f'{start_time.year}-{start_time.month:02d}-{start_time.day:02d}'
+    time = f'{start_time.hour:02d}-{start_time.minute:02d}'
+    week = start_time.isocalendar()[1]
+
+    s3 = boto3.client('s3')
+    s3_response_result = s3.put_object(
+        Body=results.to_csv(index=False),
+        Bucket=bucket,
+        Key=f"{key}/result_{week:02d}_{date}_{time}.csv")
+
+    return s3_response_result
+
+
+def read_sav_from_s3(bucket, key, role_arn=None):
+    s3 = boto3.client('s3')
+    filedescriptor, sav = mkstemp(suffix='.sav')
+    with os.fdopen(filedescriptor, 'wb') as fileobj:
+        s3.download_fileobj(Bucket=bucket, Key=key, Fileobj=fileobj)
+    return joblib.load(sav)
