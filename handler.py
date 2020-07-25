119ef87884018e9548d3264d03c3abfa6cf73cff
Daniel Da Costa <daniel.pereiracosta@hotmail.com>
2020-07-25 14:44:22 -0300

    Adding model and prediction

--- handler.py
+++ handler.py
@@ -4,6 +4,7 @@ except ImportError:
     pass
 import json
 import datetime
+import pandas as pd
 from config import READ_BUCKET, \
     READ_DATA_PATH, \
     READ_MODELS_PATH, \
@@ -19,13 +20,19 @@ def lambda_handler(event, context):
     # Getting the current time
     start_time = datetime.datetime.now()
     print('Loading CSV from s3')
-    df_plan = read_csv_from_s3(READ_BUCKET, f'{READ_DATA_PATH}/teste.csv',
+    df_plan = read_csv_from_s3(READ_BUCKET, f'{READ_DATA_PATH}/test_data.csv',
                                sep=',')
-    print(df_plan)
+
+    print('Loading model from s3')
+    model = read_sav_from_s3(READ_BUCKET, f'{READ_MODELS_PATH}/xgb_model.sav')
+
+    print('Making prediction')
+    predictions = model.predict(df_plan)
+    results = pd.Series(data=predictions, index=df_plan.index)
 
     # Save the results on s3
     s3_results = save_results(WRITE_BUCKET, WRITE_DATA_PATH,
-                              df_plan.head(), start_time)
+                              results, start_time)
     print(f's3 responses results:\n{s3_results}')
 
     response = {
